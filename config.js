75da172784534246f596376f8cab18456305a598
DominicDev <s0l0estudi4r@gmail.com>
2026-05-23 23:18:00 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'THE ORAL CIGARRETES';
+export const nameSongs = [
+	"HEY KIDS!!", /*1*/
+	"BLACK MEMORY", /*2*/
+	"BABY", /*3*/
+	"KISSES AND KILLS", /*4*/
+	"REVISIONS", /*5*/
+	"MVIP", /*6*/
+	"STORY", /*7*/
+	"5150", /*8*/
+	"MERGE", /*9*/
+	"A-E-U-I", /*10*/
+	"KYOURAN HEY KIDS!!", /*11*/
+	"SHALALA", /*12*/
+	"DIP-BAP", /*13*/
+	"BEFORE IT'S TOO LATE", /*14*/
+	"KISSES AND KILLS", /*15*/
+	"STARGET", /*16*/
+	"CATCH ME", /*17*/
+	"ONE'S AGAIN", /*18*/
+	"SUCK MY WORLD", /*19*/
+	"RED CRIMINAL", /*20*/
+]
